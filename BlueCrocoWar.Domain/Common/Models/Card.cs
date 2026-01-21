4d5a547d894e3118d8193ed555fa7654f236bb45
GKostov0 <georgi.kostov0101@gmail.com>
2026-01-21 02:44:33 +0200

    add initial projects setup

---
+++ BlueCrocoWar.Domain/Common/Models/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueCrocoWar.Domain.Common.Models
+{
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Rank Rank { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ BlueCrocoWar/Hubs/WarGame.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlueCrocoWar.Hubs;
+
+public sealed class WarGame : Hub
+{
+    public async Task NewMessage(long username, string message) =>
+        await Clients.All.SendAsync("messageReceived", username, message);
+}
