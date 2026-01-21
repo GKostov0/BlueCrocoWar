aa7789ce31246548e97012d06120c5436ffb85ef
GKostov0 <georgi.kostov0101@gmail.com>
2026-01-21 14:27:55 +0200

    add player model

---
+++ BlueCrocoWar.Domain/Common/Enums/GameStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace BlueCrocoWar.Domain.Common.Enums
+{
+    public enum GameStatus 
+    {
+        Waiting = 0,
+        InProgress = 1,
+        Finished = 2
+    }
+}
---
+++ BlueCrocoWar.Domain/Common/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace BlueCrocoWar.Domain.Common.Models
+{
+    public class Player
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+        public int PlayerNumber { get; set; }
+        public Queue<Card> Deck { get; set; }
+        public Card CurrentCard { get; set; }
+        public int TimeBank { get; set; }
+        public bool IsConnected { get; set; }
+    }
+}
